71b1cf420cee80417b1dbdce9c3284358a8c1dc9
ignacio_vazquez_pena <ignacio_vazquez_pena_ivazquezpena@icloud.com>
2021-01-14 11:25:30 +0100

    Actualizacion de BD
    Se actualiación de BD

--- Sharp_view.sql
+++ Sharp_view.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE  IF NOT EXISTS `web` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `Sharp_View`;
--- MySQL dump 10.13  Distrib 8.0.22, for Linux (x86_64)
+CREATE DATABASE  IF NOT EXISTS `Sharp_view` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `Sharp_view`;
+-- MySQL dump 10.13  Distrib 8.0.21, for macos10.15 (x86_64)
 --
--- Host: 127.0.0.1    Database: web
+-- Host: 127.0.0.1    Database: Sharp_view
 -- ------------------------------------------------------
--- Server version	8.0.22-0ubuntu0.20.04.3
+-- Server version	8.0.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -27,19 +27,22 @@ DROP TABLE IF EXISTS `aspecto`;
 CREATE TABLE `aspecto` (
   `idaspecto` int NOT NULL AUTO_INCREMENT,
   `puesto` varchar(20) NOT NULL,
-  `año_inicio` timestamp NOT NULL,
-  `año_fin` timestamp NOT NULL,
-  `accesibilidad` varchar(5) NOT NULL,
-  `ambiente_de_trabajo` varchar(5) NOT NULL,
-  `sueldos` varchar(5) NOT NULL,
-  `posibilidad_de_ascenso` varchar(5) NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `fecha_fin` date DEFAULT NULL,
+  `accesibilidad` int DEFAULT NULL,
+  `ambiente_de_trabajo` int DEFAULT NULL,
+  `sueldos` int DEFAULT NULL,
+  `posibilidad_de_ascenso` int DEFAULT NULL,
+  `opinion` varchar(200) DEFAULT NULL,
+  `conciliacion` int DEFAULT NULL,
+  `estabilidad` int DEFAULT NULL,
+  `validacion` tinyint NOT NULL DEFAULT '0',
+  `idusuario` int NOT NULL,
   `idempresa` int NOT NULL,
-  `opinion` varchar(200) NOT NULL,
-  `conciliacion` varchar(20) DEFAULT NULL,
-  `estabilidad` varchar(20) NOT NULL,
-  PRIMARY KEY (`idaspecto`),
-  KEY `idempresa` (`idempresa`),
-  CONSTRAINT `idempresa` FOREIGN KEY (`idempresa`) REFERENCES `empresa` (`idempresa`)
+  PRIMARY KEY (`idaspecto`,`idusuario`,`idempresa`),
+  KEY `fk_aspecto_usuario1_idx` (`idusuario`),
+  KEY `fk_aspecto_empresa1_idx` (`idempresa`),
+  CONSTRAINT `fk_aspecto_usuario1` FOREIGN KEY (`idusuario`) REFERENCES `usuario` (`idusuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -61,21 +64,14 @@ DROP TABLE IF EXISTS `empresa`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `empresa` (
   `idempresa` int NOT NULL AUTO_INCREMENT,
-  `sede` varchar(25) NOT NULL,
-  `accesibilidad` tinyint DEFAULT NULL,
-  `ambiente_de_trabajo` tinyint DEFAULT NULL,
-  `sueldos` tinyint DEFAULT NULL,
-  `posibilidad_de_ascenso` tinyint DEFAULT NULL,
-  `bio` varchar(155) NOT NULL,
-  `link` varchar(255) NOT NULL,
+  `nombre_empresa` varchar(45) DEFAULT NULL,
+  `sede` varchar(25) DEFAULT NULL,
+  `bio` varchar(155) DEFAULT NULL,
+  `link` varchar(255) DEFAULT NULL,
   `idusuario` int NOT NULL,
-  `opinion` varchar(200) NOT NULL,
-  `conciliacion` tinyint DEFAULT NULL,
-  `estabilidad` tinyint DEFAULT NULL,
-  PRIMARY KEY (`idempresa`),
-  KEY `idusuario` (`idusuario`),
-  CONSTRAINT `idusuario` FOREIGN KEY (`idusuario`) REFERENCES `usuario` (`idusuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`idempresa`,`idusuario`),
+  KEY `fk_empresa_usuario1_idx` (`idusuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -84,6 +80,7 @@ CREATE TABLE `empresa` (
 
 LOCK TABLES `empresa` WRITE;
 /*!40000 ALTER TABLE `empresa` DISABLE KEYS */;
+INSERT INTO `empresa` VALUES (7,'Hack a boss','Santiago','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tincidunt tortor diam, ac imperdiet lectus blandit vel.',NULL,2);
 /*!40000 ALTER TABLE `empresa` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -97,16 +94,17 @@ DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE `usuario` (
   `idusuario` int NOT NULL AUTO_INCREMENT,
   `email` varchar(50) NOT NULL,
-  `clave` varchar(10) NOT NULL,
-  `nombre` varchar(25) NOT NULL,
-  `pais` varchar(50) NOT NULL,
-  `ciudad` varchar(10) NOT NULL,
-  `bio` varchar(55) NOT NULL,
-  `link` varchar(255) NOT NULL,
-  `empresa` tinyint DEFAULT NULL,
-  `empleado` tinyint DEFAULT NULL,
+  `clave` varchar(255) NOT NULL,
+  `nombre` varchar(25) DEFAULT NULL,
+  `pais` varchar(50) DEFAULT NULL,
+  `ciudad` varchar(10) DEFAULT NULL,
+  `bio` varchar(55) DEFAULT NULL,
+  `link` varchar(255) DEFAULT NULL,
+  `nombre_empresa` tinyint DEFAULT NULL,
+  `empresa` tinyint DEFAULT '0',
+  `empleado` tinyint DEFAULT '0',
   PRIMARY KEY (`idusuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -115,6 +113,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'jose@email.com','$2a$10$z2EZnfaAJwbmd1X3gGDpeeaww8qEidC/OndP.EUbridLD2MPUgyjC',NULL,NULL,NULL,NULL,NULL,NULL,0,0),(2,'hackaboss@email.com','$2a$10$0Mzi78krYXaVSzjeprbG6Og2Ig6464lotjDJM8aauHgGQxmXkN5be',NULL,NULL,NULL,NULL,NULL,NULL,1,0);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -127,4 +126,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-13 16:50:33
+-- Dump completed on 2021-01-14 11:18:23
