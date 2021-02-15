3dabed9aa8fa7f04bca1ef169b7e7b7f28364dc6
mariaareoso <mariaareoso@hotmail.com>
2021-02-15 20:36:03 +0100

    Añadidos componentes

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,13 +1,15 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { AuthProvider } from './shared/context/authContext';
-import PrivateRoute from './components/PrivateRoute';
-import Home from './pages/Home';
-import EmpresasList from './pages/EmpresasList';
-import EmpresaInfo from './pages/EmpresaInfo';
-import RegisterHome from './pages/RegisterHome';
-import PerfilUser from './pages/PerfilUsuario';
-
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { AuthProvider } from "./shared/context/authContext";
+import PrivateRoute from "./components/PrivateRoute";
+import Home from "./pages/Home";
+import EmpresasList from "./pages/EmpresasList";
+import EmpresaInfo from "./pages/EmpresaInfo";
+import RegisterHome from "./pages/RegisterHome";
+import PerfilUser from "./pages/PerfilUsuario";
+import AjustesDeCuenta from "./pages/AjustesDeCuenta";
+import ResetPassword from "./components/ajustesDeCuenta/resetPassword";
+import VentanaTablaEmpleados from "./components/listaEmpleados/WorkerList";
+import ValoracionTablaEmpleados from "./components/valoracionesEmpleados/voteList";
 
 function App() {
   return (
@@ -31,6 +33,18 @@ function App() {
               <PerfilUser></PerfilUser>
             </PrivateRoute>
           </Route>
+          <Route exact path="/changeAccount">
+            <AjustesDeCuenta></AjustesDeCuenta>
+          </Route>
+          <Route path="/resetPassword">
+            <ResetPassword></ResetPassword>
+          </Route>
+          <Route path="/WorkerList">
+            <VentanaTablaEmpleados></VentanaTablaEmpleados>
+          </Route>
+          <Route path="/voteList">
+            <ValoracionTablaEmpleados></ValoracionTablaEmpleados>
+          </Route>
         </AuthProvider>
       </Switch>
     </Router>
@@ -38,4 +52,4 @@ function App() {
 }
 
 export default App;
-
+eG;
